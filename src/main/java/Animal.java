386475946972ab80918209754e1bdf2da9ea695e
aldomaximillian <e0407750@u.nus.edu>
2020-09-04 17:25:42 +0800

    in-class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,6 +1,11 @@
-public class Animal {
+public class Animal implements Asset{
     
     public String speak(){
         return null;
+    }
+
+    @Override
+    public int getValue() {
+        return 20;
     }
 }
